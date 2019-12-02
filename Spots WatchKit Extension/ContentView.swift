626ac954a26105d6b3caf55a37ee14ccf4a79d97
Guillaume Claverie <guillaume351.claverie@gmail.com>
2019-12-02 20:56:45 +0100

    Add GPS location

--- Spots WatchKit Extension/ContentView.swift
+++ Spots WatchKit Extension/ContentView.swift
@@ -7,24 +7,53 @@
 //
 
 import SwiftUI
+import MapKit
+
 
 struct ContentView: View {
+    @ObservedObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
+    
     var body: some View {
+
         
         VStack{
+            Text("Spots")
+           
+            Text("latitude: \(userLatitude)")
+            Text("longitude: \(userLongitude)")
+            Spacer()
             Button(action: {
                    print("Add button tapped!")
+                
+                
                }) {
-                   Image(systemName: "plus.circle")
-                       .font(.largeTitle)
-                       .foregroundColor(.red)
+                HStack{
+                    Image(systemName: "plus.circle")
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+                    Text("Ajouter")
+                }
+                   
                }
             Button(action: {
                 print("List button tapped!")
             }){
-                Image(systemName: "list.dash")
-                    .font(.largeTitle)
-                    .foregroundColor(.red)
+                HStack{
+                    Image(systemName: "list.dash")
+                                      .font(.largeTitle)
+                                      .foregroundColor(.red)
+                    Text("Afficher")
+                }
+              
             }
         
         
@@ -40,4 +69,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct defaultStorageKeys{
+    static let spotsKey = "storeSpotsKey"
 }
